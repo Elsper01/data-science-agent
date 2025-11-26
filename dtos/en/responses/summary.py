29943593fc7e440da0191459ebef8315f8f3a5c4
Jonas Elsper <jonas.elsper@seitenbau.com>
2025-11-26 17:23:44 +0100

    fixed import

--- dtos/en/responses/summary.py
+++ dtos/en/responses/summary.py
@@ -1,7 +1,7 @@
 from pydantic import Field
 
 from dtos.base.responses.summary_base import SummaryBase
-from dtos.de.responses.column import Column
+from dtos.en.responses.column import Column
 
 
 class Summary(SummaryBase):

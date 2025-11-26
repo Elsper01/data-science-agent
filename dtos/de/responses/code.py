7c6ddac7ed8842c14f1b176a6aa3474c2de09578
Jonas Elsper <jonas.elsper@seitenbau.com>
2025-11-26 17:22:33 +0100

    fixed descriptions

--- dtos/de/responses/code.py
+++ dtos/de/responses/code.py
@@ -2,6 +2,6 @@ from pydantic import Field
 from dtos.base.metadata_base import MetadataBase
 
 class Code(MetadataBase):
-    """Antwortformat: Python-Code, generiert vom LLM, gefolgt von einer kurzen Erklärung des Codes."""
+    """Antwortformat: Code, generiert vom LLM, gefolgt von einer kurzen Erklärung des Codes."""
     explanation: str = Field(..., description="Erklärt den Code kurz.")
-    code: str = Field(..., description="Syntactic correct python code.")
+    code: str = Field(..., description="Syntaktisch korrekter code.")

c6e94f71a4318117c0c95f741d21fd6038314b33
Jonas Elsper <jonas.elsper@seitenbau.com>
2026-01-05 13:17:42 +0100

    added new test datasets

--- src/data_science_agent/test.py
+++ src/data_science_agent/test.py
@@ -3,28 +3,35 @@ from time import time
 from data_science_agent.graph import build_graph
 from data_science_agent.utils import print_color, enums
 
-start = time()
+import os
 
-agent = build_graph()
+datasets = os.listdir("./src/resources/data/")
 
-png_bytes = agent.get_graph().draw_mermaid_png()
+for dataset in datasets:
+    start = time()
 
-with open("./src/resources/output/graph.png", "wb") as f:
-    f.write(png_bytes)
+    agent = build_graph()
 
-print("Saved graph.png successfully.")
+    png_bytes = agent.get_graph().draw_mermaid_png()
 
-result = agent.invoke(
-    {
-        "dataset_path": "./src/resources/data/pegel.csv",
-        "metadata_path": "./src/resources/data/pegel.rdf",
-        "regeneration_attempts": 0,
-        "programming_language": enums.ProgrammingLanguage.R,
-        "is_refactoring": False,
-        "output_path": "./src/resources/output/",
-        "durations": [],
-        "llm_metadata": [],
-    }
-)
+    with open("./src/resources/output/graph.png", "wb") as f:
+        f.write(png_bytes)
 
-print_color(f"Agent finished in {time() - start} seconds.", enums.Color.WARNING)
+    print("Saved graph.png successfully.")
+
+    path = os.path.join("./src/resources/data/", dataset)
+
+    result = agent.invoke(
+        {
+            "dataset_path": os.path.join(path, "data.csv"),
+            "metadata_path": os.path.join(path, "metadata.rdf"),
+            "regeneration_attempts": 0,
+            "programming_language": enums.ProgrammingLanguage.R,
+            "is_refactoring": False,
+            "output_path": "./src/resources/output/",
+            "durations": [],
+            "llm_metadata": [],
+        }
+    )
+
+    print_color(f"Agent finished in {time() - start} seconds.", enums.Color.WARNING)

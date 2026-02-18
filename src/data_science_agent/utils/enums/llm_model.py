5eab051ece05de53430e16870fe138bc35a4afe1
Jonas Elsper <jonas.elsper@seitenbau.com>
2026-02-18 11:26:10 +0100

    added new models

--- src/data_science_agent/utils/enums/llm_model.py
+++ src/data_science_agent/utils/enums/llm_model.py
@@ -3,9 +3,10 @@ from enum import StrEnum
 class LLMModel(StrEnum):
     """Contains all supported and tested LLM models"""
     GPT_4o = "gpt-4o"
-    GPT_5 = "gpt-5"
+    GPT_5 = "gpt-5" # nehmen wir
     GROK = "x-ai/grok-code-fast-1"
-    CLAUDE_4 = "anthropic/claude-sonnet-4.5"
+    CLAUDE_4 = "anthropic/claude-sonnet-4.5" # nehmen wir
+    GEMINI = "google/gemini-3-flash-preview" # nehmen wir
     MINIMAX = "minimax/minimax-m2.1"
     MISTRAL = "mistralai/mistral-large-2512"
     DEVSTRAL = "mistralai/devstral-2512:free"
